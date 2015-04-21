3599465280f973b12af18dee36393d85d9238e93
Lindsay Carbonell <lindsaycarbonell@dhcp-v880-13-00875.1Xwireless.unc.edu>
2015-04-21 13:43:04 -0400

    got lots of stuff working

---
+++ tumblrapi 1.php
@@ -0,0 +1,11 @@
+<?php
+include ("tumblrPHP.php");
+// Enter your Consumer (Also known as oyur API KEY)
+$consumer = "LS3z3S1Tce1RbL4jvypkv28as3cbH9VIOvVuy6GbQia5zwrkde";
+// Create a new instance of the Tumblr Class with your Conumser when you create your app. You can pass an empty string for the secret, or you can add it.
+$tumblr = new Tumblr($consumer, "");
+
+// Grab the followers by using the oauth_get method.
+$info = $tumblr->get("/blog/blog.maxora.tumblr.com/info");
+print_r($info);
+?>
